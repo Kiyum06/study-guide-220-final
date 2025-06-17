92881c01c7006536f79b7ed33d8bc0907de46309
Kiyum-06 <kiyum1406@gmail.com>
2025-06-17 07:41:23 -0700

    Done with 1st method and tests.

--- src/FinalPractice.java
+++ src/FinalPractice.java
@@ -1,8 +1,24 @@
 public class FinalPractice {
     public static void main(String[] args) {
+
         // Feel free to call your methods here to experiment
 
-        // YOU MUST ALSO MAKE THOROUGH TESTS FOR EVERY METHOD 
+        // 1st method
+        // Creating the linked list: 4 -> 8 -> 15 -> 16 -> 23 -> 42 -> 11 -> 29 -> 34
+        ListNode list = new ListNode(4,
+                new ListNode(8,
+                        new ListNode(15,
+                                new ListNode(16,
+                                        new ListNode(23,
+                                                new ListNode(42,
+                                                        new ListNode(11,
+                                                                new ListNode(29,
+                                                                        new ListNode(34)))))))));
+
+        int result = oddIndexSum(list);
+        System.out.println("Sum of elements at odd indexes: " + result);
+
+        // YOU MUST ALSO MAKE THOROUGH TESTS FOR EVERY METHOD
     }
 
     /**
@@ -17,7 +33,23 @@ public class FinalPractice {
      */
     public static int oddIndexSum(ListNode head) {
         // TODO: implement this AND MAKE MORE UNIT TESTS FOR IT
-        return -1;
+        if (head == null)
+            return 0;
+
+        int sum = 0;
+        int index = 0;
+        ListNode current = head;
+
+        while (current != null) {
+            if (index % 2 == 1) {
+                sum += current.data;
+            }
+            current = current.next;
+            index++;
+        }
+
+        return sum;
+
     }
 
     // TODO: implement the rest of the study guide AND MAKE GOOD UNIT TESTS
--- src/FinalPracticeTest.java
+++ src/FinalPracticeTest.java
@@ -5,12 +5,69 @@ import org.junit.jupiter.api.Test;
 public class FinalPracticeTest {
     @Test
     void testOddIndexSum_oddLengthList() {
-        ListNode list = new ListNode(4, new ListNode(8, new ListNode(15, new ListNode(16, new ListNode(23, new ListNode(42, new ListNode(11, new ListNode(29, new ListNode(34)))))))));
+        ListNode list = new ListNode(4, new ListNode(8, new ListNode(15, new ListNode(16,
+                new ListNode(23, new ListNode(42, new ListNode(11, new ListNode(29, new ListNode(34)))))))));
         int actual = FinalPractice.oddIndexSum(list);
         assertEquals(95, actual);
     }
 
     // TODO: Make more tests for oddIndexSum
+    @Test
+    void testOddIndexSum_nullList() {
+        ListNode list = null;
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void testOddIndexSum_singleNode() {
+        ListNode list = new ListNode(5);
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void testOddIndexSum_twoNodes() {
+        ListNode list = new ListNode(10, new ListNode(20));
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(20, actual);
+    }
+
+    @Test
+    void testOddIndexSum_evenLengthList() {
+        ListNode list = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5,
+                                                new ListNode(6))))));
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(2 + 4 + 6, actual); // 12
+    }
+
+    @Test
+    void testOddIndexSum_alternatingZero() {
+        ListNode list = new ListNode(1,
+                new ListNode(0,
+                        new ListNode(3,
+                                new ListNode(0,
+                                        new ListNode(5)))));
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(0 + 0, actual); // 0
+    }
+
+    @Test
+    void testOddIndexSum_negativeValuesAtOddIndexes() {
+        ListNode list = new ListNode(10,
+                new ListNode(-2,
+                        new ListNode(30,
+                                new ListNode(-4,
+                                        new ListNode(50,
+                                                new ListNode(-6))))));
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(-2 + -4 + -6, actual); // -12
+    }
 
     // TODO: Make thorough tests for ALL the questions on the study guide
+
 }
